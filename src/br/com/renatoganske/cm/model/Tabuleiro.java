307612cde2d23f4285eb54c4cd7dd0e2d9404f94
renatoganske <renatoganske.jr@gmail.com>
2022-07-18 23:27:03 -0300

    feature: Implementação dos métodos gerarCampos e associarVizinhos na classe Tabuleiro.

--- src/br/com/renatoganske/cm/model/Tabuleiro.java
+++ src/br/com/renatoganske/cm/model/Tabuleiro.java
@@ -1,5 +1,44 @@
 package br.com.renatoganske.cm.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+
+	private int qntLinhas;
+	private int qntColunas;
+	private int qntMinas;
+
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int qntLinhas, int qntColunas, int qntMinas) {
+		this.qntLinhas = qntLinhas;
+		this.qntColunas = qntColunas;
+		this.qntMinas = qntMinas;
+
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+	}
+
+	private void gerarCampos() {
+		for (int l = 0; l < qntLinhas; l++) {
+			for (int c = 0; c < qntColunas; c++) {
+				campos.add(new Campo(l, c));
+			}
+		}
+	}
+
+	private void associarVizinhos() {
+		for (Campo c1 : campos) {
+			for (Campo c2 : campos) {
+				c1.adicionarVizinho(c2);
+			}
+		}
+	}
+
+	private void sortearMinas() {
+
+	}
 
 }
